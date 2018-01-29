8b096469dc3594e6628c0d0b622d2f47a512628a
Michael Flis <michaelflis07@gmail.com>
2018-01-29 12:20:16 -0800

    Render Views for App, video list, player  and videos

--- src/views/app.js
+++ src/views/app.js
@@ -4,14 +4,25 @@ var AppView = Backbone.View.extend({
 
   initialize: function() {
     this.videos = new Videos();
+    this.videoPlayerView = new VideoPlayerView;
+    this.videoListView = new VideoListView;
+    this.videoListView.render()
+    this.videoPlayerView.render();
+
+    this.render();
+
+    console.log(this)
+
   },
 
 
   render: function() {
     this.$el.html(this.template());
+    console.log(this.$el);
     return this;
   },
 
   template: templateURL('src/templates/app.html')
 
 });
+
