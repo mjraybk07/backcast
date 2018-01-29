9c999bb244fb135cb61618e15cae297b5862ee94
Michael Flis <michaelflis07@gmail.com>
2018-01-29 14:10:20 -0800

    EOD

--- src/views/app.js
+++ src/views/app.js
@@ -3,26 +3,26 @@ var AppView = Backbone.View.extend({
   el: '#app',
 
   initialize: function() {
-    this.videos = new Videos();
-    this.videoPlayerView = new VideoPlayerView;
-    this.videoListView = new VideoListView;
-    this.videoListView.render()
-    this.videoPlayerView.render();
+    this.videos = new Videos(window.exampleVideoData);
+    console.log(this.videos);
 
+    this.videoPlayerView = new VideoPlayerView({collection: this.videos});
+    this.videoListView = new VideoListView({collection: this.videos});
+    //this.videoListView.render()
+    //this.videoPlayerView.render();
     this.render();
-
-    console.log(this)
 
   },
 
 
   render: function() {
     this.$el.html(this.template());
-    console.log(this.$el);
     return this;
   },
 
   template: templateURL('src/templates/app.html')
 
 });
+
+
 
--- src/views/videoList.js
+++ src/views/videoList.js
@@ -1,5 +1,19 @@
 var VideoListView = Backbone.View.extend({
 
+  initialize: function() {
+    // Render the VideoListView
+    this.render();
+
+    console.log('inside VideoListView.initialize....collection:', this.collection);
+
+
+    // Render each VideoListEntry
+    this.collection.forEach(function(element) {
+      var video = new VideoListEntryView({model: element});
+      video.render();
+    });
+
+  },
 
   render: function() {
     this.$el.children().detach();
